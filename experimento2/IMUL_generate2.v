e1dfa0122ffd1126184835a53a3a075bdc22f056
Luis Mora <lmoramora94@gmail.com>
2017-04-19 10:43:30 -0600

    Generate primera fila

--- experimento2/IMUL_generate2.v
+++ experimento2/IMUL_generate2.v
@@ -10,7 +10,9 @@ input wire [15:0] A;
 input wire [15:0] B;
 output [31:0] oResult;
 
-wire[2:0] wCarry[2:0];
+wire wCarry[3:0][15:0];
+wire wResult[3:0][15:0];
+
 
 
 genvar i, j;
@@ -21,8 +23,15 @@ assign oResult[0] = A[0] & B[0];
 
 for(i = 0; i< 15; i = i+1) begin
 	for(j = 0; i < 30; j = j+1) begin
+
+		assign wCarry[j][ 0 ] = 0;
+
 		if(i == 0) begin
-			full_adder fa1(); // A, B, Ci, R, Co
+			full_adder fa00(.A(A[1]&B[0]), .B(A[0]&B[1]), .Ci(1'b0), .R(oResult[1]), .Co(wCarry[0][0])); // A, B, Ci, R, Co
+			full_adder fa01(.A(A[1]&B[1]), .B(A[2]&B[0]), .Ci(wCarry[0][0]), .R(wResult[0][1]), .Co(wCarry[0][1])); // A, B, Ci, R, Co
+			full_adder fa02(.A(A[2]&B[1]), .B(A[3]&B[0]), .Ci(wCarry[0][1]), .R(wResult[0][2]), .Co(wCarry[0][2])); // A, B, Ci, R, Co
+			full_adder fa03(.A(A[3]&B[1]), .B(1'b0), .Ci(wCarry[0][2]), .R(wResult[0][3]), .Co(wCarry[0][3])); // A, B, Ci, R, Co
+
 		end
 
 	end
