f34d73e9b3095211b91893850725736fbd83296b
Luis Diego Fernandez Coto <luisdifc1993@gmail.com>
2017-05-31 10:58:19 -0600

    fsm vga

--- experimento4/Collaterals.v
+++ experimento4/Collaterals.v
@@ -181,169 +181,55 @@ endmodule
 
 //----------------------------------------------------------------------
 
-module LCD(
-	clk,
-	chars,
-	//nibble,
-	//counter,
-	lcd_rs, lcd_rw, lcd_e, lcd_4, lcd_5, lcd_6, lcd_7);
+module VGA_SYNC (oVsync, oHsync, oRed, oGreen, oBlue, CLK, iColors[2:0]);
 
-	// inputs and outputs
-	input       	clk;
-	//input [3:0] nibble;
-	//input [6:0] counter;
-	// inputreg [256:0] 	chars;
-	output      	lcd_rs, lcd_rw, lcd_e, lcd_4, lcd_5, lcd_6, lcd_7;
+	output reg [9:0] oVsync;
+	output reg [9:0] oHsync;
+	output reg oRed, oGreen, oBlue;
 
-	input wire [256:0] 	chars;
-	reg	 	lcd_rs, lcd_rw, lcd_e, lcd_4, lcd_5, lcd_6, lcd_7;
+	input wire CLK;
+	input wire [2:0] iColors;
 
-	// internal variables
-	reg [5:0] 	lcd_code;
-	reg [1:0] 	write = 2'b10;	// write code has 10 for rs rw
+	reg [19:0] counter_display; 
+	reg [1:0] vga_state;
 
-	// delays
-	reg [1:0]	before_delay = 3;	// time before on
-	reg [3:0]	on_delay = 13;		// time on
-	reg [23:0]	off_delay = 750_001;	// time off
-
-	// states and counters
-	reg [6:0]	current_State = 0;
-	reg [19:0]	count = 0;
-	reg [1:0]	delay_state = 0;
-
-	// character data
-	reg [256:0]	chars_hold = "                                ";
-	wire [3:0]	chars_data [63:0];	// array of characters
-
-
-
-	// redirects characters data to an array
-	generate
-	genvar i;
-		for (i = 64; i > 0; i = i-1)
-			begin : for_name
-				assign chars_data[64-i] = chars_hold[i*4-1:i*4-4];
-			end
-	endgenerate
+	assign vga_state = 0;
+	assign next_state_vga = 0;
 
 	always @ (posedge clk) begin
+		case (vga_state)
+			0: begin 
+				oVsync <= 10'b0;
+				oHsync <= 10'b0;
+				oRed <= 1;
+				oGreen <= 0;
+				oBlue <= 0;
+				counter_display <= 0;
+				vga_state <= 1;
+				end //end 0
 
-		// store character data
-		if (current_State == 10 && count == 0)
-			chars_hold <= chars;
-			//chars_data [counter] <= nibble;
-		
-		// set time when enable is off
-		//INICIALIZACION
-		if (current_State < 3) begin
-			case (current_State)
-				0: off_delay <= 750000;	// 15ms delay
-				1: off_delay <= 250000;	// 5ms delay
-				2: off_delay <= 5000;		// 0.1ms delay
-			endcase
-		end else begin
-			if (current_State > 12) begin
-				off_delay	<= 2000;	// 40us delay
-			end else begin
-				off_delay	<= 250000;	// 5ms delay
-			end
-		end
-
-		// delays during each state
-		if (current_State < 80) begin
-		case (delay_state)
-			0: begin
-					// enable is off
-					lcd_e <= 0;
-					{lcd_rs,lcd_rw,lcd_7,lcd_6,lcd_5,lcd_4} <= lcd_code;
-					if (count == off_delay) begin
-						count <= 0;
-						delay_state <= delay_state + 1;
-					end else begin
-						count <= count + 1;
-					end
-				end
 			1: begin
-					// data set before enable is on
-					lcd_e <= 0;
-					if (count == before_delay) begin
-						count <= 0;
-						delay_state <= delay_state + 1;
+				if (counter_display < 850000)
+					counter_display <= counter_display + 1;
+				else begin
+					oRed <= 1;
+					oGreen <= 0;
+					oBlue <= 0;
+					counter_display <= 0;
+					vga_state <= 1;
+					if (oVsync == 639) begin
+						if (oHsync == 479) begin
+							oVsync <= 10'b0;
+							oHsync <= 10'b0;
+						end else begin
+							oHsync <= oHsync + 1;
+							oVsync <= 0;							
+						end //else
 					end else begin
-						count <= count + 1;
-					end
-				end
-			2: begin
-					// enable on
-					lcd_e <= 1;
-					if (count == on_delay) begin
-						count <= 0;
-						delay_state <= delay_state + 1;
-					end else begin
-						count <= count + 1;
-					end
-				end
-			3: begin
-					// enable off with data set
-					lcd_e <= 0;
-					if (count == before_delay) begin
-						count <= 0;
-						delay_state <= 0;
-						current_State <= current_State + 1;		// next case
-					end else begin
-						count <= count + 1;
-					end
-				end
+						oVsync <= oVsync + 1;
+					end //else
+				end //else
+			end //begin
 		endcase
-		end
-
-		// set lcd_code
-		if (current_State < 12) begin
-			// initialize LCD
-			case (current_State)
-				0: lcd_code <= 6'h03;        // power-on initialization
-				1: lcd_code <= 6'h03;
-				2: lcd_code <= 6'h03;
-				3: lcd_code <= 6'h02;
-				4: lcd_code <= 6'h02;        // function set
-				5: lcd_code <= 6'h08;
-				6: lcd_code <= 6'h00;        // entry mode set
-				7: lcd_code <= 6'h06;
-				8: lcd_code <= 6'h00;        // display on/off control
-				9: lcd_code <= 6'h0C;
-				10:lcd_code <= 6'h00;        // display clear
-				11:lcd_code <= 6'h01;
-				default: lcd_code <= 6'h10;	
-			endcase
-		end else begin
-
-			// set character data to lcd_code
-			if (current_State == 44) begin			// change address at end of first line
-				lcd_code <= {2'b00, 4'b1100};	// 0100 0000 address change
-			end else if (current_State == 45) begin
-				lcd_code <= {2'b00, 4'b0000};
-			end else begin
-				if (current_State < 44) begin
-					lcd_code <= {write, chars_data[current_State-12]};
-				end else begin
-					lcd_code <= {write, chars_data[current_State-14]};
-				end
-			end
-
-		end
-
-		// hold and loop back
-		if (current_State == 78) begin
-			lcd_e <= 0;
-			if (count == off_delay) begin
-				current_State 			<= 10;
-				count 		<= 0;
-			end else begin
-				count <= count + 1;
-			end
-		end
-
-	end
-
+	end //end always @ posedge
 endmodule
