0ec1786a237266d734587427f64f1ea2c568b2ae
LennonNM <lennonnuez@gmail.com>
2017-06-28 11:32:06 -0600

    su madre

---
+++ Boton/Collaterals.v
@@ -0,0 +1,356 @@
+`timescale 1ns / 1ps
+
+//------------------------------------------------
+// Módulo del contador que se incrementa en 1 para 
+// llevar el orden de las instrucciones a ejecutar.
+module UPCOUNTER_POSEDGE # (parameter SIZE=16)
+(
+input wire Clock, Reset,
+input wire [SIZE-1:0] Initial,
+input wire Enable,
+output reg [SIZE-1:0] Q
+);
+
+  always @(posedge Clock )
+  begin
+      if (Reset)
+        Q = Initial;
+      else
+		begin
+		if (Enable)
+			Q = Q + 1;
+			
+		end			
+  end
+
+endmodule
+//----------------------------------------------------
+// Módulo de un flip-flop D de flanco positivo 
+module FFD_POSEDGE_SYNCRONOUS_RESET # ( parameter SIZE=8 )
+(
+	input wire				Clock,
+	input wire				Reset,
+	input wire				Enable,
+	input wire [SIZE-1:0]	D,
+	output reg [SIZE-1:0]	Q
+);
+	
+always @ (posedge Clock) 
+begin
+	if ( Reset )
+		Q <= 0;
+	else
+	begin	
+		if (Enable) 
+			Q <= D; 
+	end	
+ 
+end//always
+
+endmodule
+//----------------------------------------------------------------------
+// Módulo VGA controller
+module VGA_controller
+(
+	input wire				Clock_lento,
+	input wire 				Reset,
+	input wire	[2:0]		iVGA_RGB,
+	input wire  [2:0]		iColorCuadro,
+	input wire  [7:0]		iXRedCounter,
+	input wire  [7:0]		iYRedCounter,
+	output wire	[2:0]		oVGA_RGB,
+	output wire				oHsync,
+	output wire				oVsync,
+	output wire [9:0]		oVcounter,
+	output wire [9:0]		oHcounter
+);
+wire iVGA_R, iVGA_G, iVGA_B;
+wire oVGA_R, oVGA_G, oVGA_B;
+wire wEndline;
+wire [3:0] wMarco; //, wCuadro;
+wire [2:0] wVGAOutputSelection;
+
+assign wMarco = 3'b0;
+
+assign wVGAOutputSelection = {iVGA_R, iVGA_G, iVGA_B};
+
+assign iVGA_R = iVGA_RGB[2];
+assign iVGA_G = iVGA_RGB[1];
+assign iVGA_B = iVGA_RGB[0];
+assign oVGA_RGB = {oVGA_R, oVGA_G, oVGA_B};
+
+assign oHsync = (oHcounter < 704) ? 1'b1 : 1'b0; //640
+assign wEndline = (oHcounter == 799);
+assign oVsync = (oVcounter < 519) ? 1'b1 : 1'b0; //480
+
+// Marco negro e imagen de 256*256
+assign {oVGA_R, oVGA_G, oVGA_B} = (oVcounter < 142 || oVcounter >= 398 || 
+					  oHcounter < 242 || oHcounter > 498) ? 
+					  wMarco : wVGAOutputSelection;
+
+UPCOUNTER_POSEDGE # (10) HORIZONTAL_COUNTER
+(
+.Clock	(   Clock_lento   ), 
+.Reset	( (oHcounter > 799) || Reset 		),
+.Initial	( 10'b0  			),
+.Enable	(  1'b1				),
+.Q			(	oHcounter      )
+);
+
+UPCOUNTER_POSEDGE # (10) VERTICAL_COUNTER
+(
+.Clock	( Clock_lento    ), 
+.Reset	( (oVcounter > 520) || Reset ),
+.Initial	( 10'b0  			),	
+.Enable	( wEndline            ),
+.Q			( oVcounter      )
+);
+
+endmodule
+
+//----------------------------------------------------------------------
+// Module LCD
+
+module LCD(
+	clk,
+	chars,
+	lcd_rs, lcd_rw, lcd_e, lcd_4, lcd_5, lcd_6, lcd_7);
+
+	// inputs and outputs
+	input clk;
+	output lcd_rs, lcd_rw, lcd_e, lcd_4, lcd_5, lcd_6, lcd_7;
+
+	input wire [256:0] 	chars;
+	reg	 	lcd_rs, lcd_rw, lcd_e, lcd_4, lcd_5, lcd_6, lcd_7;
+
+	// internal variables
+	reg [5:0] 	lcd_code;
+	reg [1:0] 	write = 2'b10;	// write code has 10 for rs rw
+
+	// delays
+	reg [1:0]	before_delay = 3;	// time before on
+	reg [3:0]	on_delay = 13;		// time on
+	reg [23:0]	off_delay = 750_001;	// time off
+
+	// states and counters
+	reg [6:0]	current_State = 0;
+	reg [19:0]	count = 0;
+	reg [1:0]	delay_state = 0;
+
+	// character data
+	reg [256:0]	chars_hold = "                                ";
+	wire [3:0]	chars_data [63:0];	// array of characters
+
+	// redirects characters data to an array
+	generate
+	genvar i;
+		for (i = 64; i > 0; i = i-1)
+			begin : for_name
+				assign chars_data[64-i] = chars_hold[i*4-1:i*4-4];
+			end
+	endgenerate
+
+	always @ (posedge clk) begin
+
+		// store character data
+		if (current_State == 10 && count == 0)
+			chars_hold <= chars;
+			//chars_data [counter] <= nibble;
+		
+		// set time when enable is off
+		//INICIALIZACION
+		if (current_State < 3) begin
+			case (current_State)
+				0: off_delay <= 750000;	// 15ms delay
+				1: off_delay <= 250000;	// 5ms delay
+				2: off_delay <= 5000;		// 0.1ms delay
+			endcase
+		end else begin
+			if (current_State > 12) begin
+				off_delay	<= 2000;	// 40us delay
+			end else begin
+				off_delay	<= 250000;	// 5ms delay
+			end
+		end
+
+		// delays during each state
+		if (current_State < 80) begin
+		case (delay_state)
+			0: begin
+					// enable is off
+					lcd_e <= 0;
+					{lcd_rs,lcd_rw,lcd_7,lcd_6,lcd_5,lcd_4} <= lcd_code;
+					if (count == off_delay) begin
+						count <= 0;
+						delay_state <= delay_state + 1;
+					end else begin
+						count <= count + 1;
+					end
+				end
+			1: begin
+					// data set before enable is on
+					lcd_e <= 0;
+					if (count == before_delay) begin
+						count <= 0;
+						delay_state <= delay_state + 1;
+					end else begin
+						count <= count + 1;
+					end
+				end
+			2: begin
+					// enable on
+					lcd_e <= 1;
+					if (count == on_delay) begin
+						count <= 0;
+						delay_state <= delay_state + 1;
+					end else begin
+						count <= count + 1;
+					end
+				end
+			3: begin
+					// enable off with data set
+					lcd_e <= 0;
+					if (count == before_delay) begin
+						count <= 0;
+						delay_state <= 0;
+						current_State <= current_State + 1;		// next case
+					end else begin
+						count <= count + 1;
+					end
+				end
+		endcase
+		end
+
+		// set lcd_code
+		if (current_State < 12) begin
+			// initialize LCD
+			case (current_State)
+				0: lcd_code <= 6'h03;        // power-on initialization
+				1: lcd_code <= 6'h03;
+				2: lcd_code <= 6'h03;
+				3: lcd_code <= 6'h02;
+				4: lcd_code <= 6'h02;        // function set
+				5: lcd_code <= 6'h08;
+				6: lcd_code <= 6'h00;        // entry mode set
+				7: lcd_code <= 6'h06;
+				8: lcd_code <= 6'h00;        // display on/off control
+				9: lcd_code <= 6'h0C;
+				10:lcd_code <= 6'h00;        // display clear
+				11:lcd_code <= 6'h01;
+				default: lcd_code <= 6'h10;	
+			endcase
+		end else begin
+
+			// set character data to lcd_code
+			if (current_State == 44) begin			// change address at end of first line
+				lcd_code <= {2'b00, 4'b1100};	// 0100 0000 address change
+			end else if (current_State == 45) begin
+				lcd_code <= {2'b00, 4'b0000};
+			end else begin
+				if (current_State < 44) begin
+					lcd_code <= {write, chars_data[current_State-12]};
+				end else begin
+					lcd_code <= {write, chars_data[current_State-14]};
+				end
+			end
+
+		end
+
+		// hold and loop back
+		if (current_State == 78) begin
+			lcd_e <= 0;
+			if (count == off_delay) begin
+				current_State 			<= 10;
+				count 		<= 0;
+			end else begin
+				count <= count + 1;
+			end
+		end
+
+	end
+
+endmodule
+
+//----------------------------------------------------------------------
+// Module BTN
+
+module BTN
+(	input wire BTN_UP,
+	input wire BTN_DOWN,
+	input wire BTN_LEFT,
+	input wire BTN_RIGHT,
+	input wire BTN_CNTR,
+	input wire CLK,
+	input wire Reset,
+	output reg [4:0] BTN
+);
+	
+	reg [3:0] rCLKCounter;  //Contador de ciclos del reloj
+	reg [4:0] rBTN; 		//Registro con botón presionado
+	reg 	  rBTNListo;	//Se mantiene en 0 cuando el botón no ha sido validado
+	reg 	  rBTNPress;		//Indica si se presionó una tecla
+	
+	//Lógica principal
+	always @ (posedge CLK) begin
+		rCLKCounter <= rCLKCounter + 1;
+		//Manejo del Reset
+		if (Reset) begin
+			rCLKCounter <= 0;
+			rBTN <= 0;
+			rBTNListo <= 0;
+			rBTNPress <= 0;
+		end	//end Reset
+		//Control de los Botones
+		else begin 
+			//Verificar que se haya presionado algún botón
+			if (BTN_UP || BTN_DOWN || BTN_LEFT || BTN_RIGHT || BTN_CNTR) begin 
+				rBTNPress <= 1;
+			end
+			else begin
+				rBTNPress <= rBTNPress;
+			end //end Verificación botón presionado
+			
+			//Supresión de Rebotes
+			if (rBTNPress) begin //Algún botón presionado
+				if (rCLKCounter >= 8) begin //Supresión de rebotes
+					rCLKCounter <= 0;
+					if (BTN_UP || BTN_DOWN || BTN_LEFT || BTN_RIGHT || BTN_CNTR) begin //Si sigue presionado
+						rBTNListo <= 1; //Botón válido
+						rBTNPress <= rBTNPress;
+						rBTN <= {BTN_UP, BTN_DOWN, BTN_LEFT, BTN_RIGHT, BTN_CNTR}; //Pasa dato del botón presionado
+					end //if sigue presionado
+					else begin
+						rBTNListo <= 0;
+					end //else falsa alarma
+				end //if contador de ciclos
+				else begin
+					rCLKCounter <= rCLKCounter;
+					rBTNListo <= rBTNListo;
+					rBTNPress <= 0;
+				end //else contador sigue contando
+			end //if BTNPress
+			else begin//No hay botón presionado
+				rBTNPress <= 0;
+				rBTNListo <= 0;
+				rCLKCounter <= 0;
+				rBTN <= 0; //Limpia rBTN			
+			end
+			
+			//Manejo salida dato del BTN
+			if (rBTNListo) begin //BTN está listo
+				BTN <= rBTN;
+				rBTNListo <=0; //Se asegura que haga esto solo una vez cuando el dato está listo y no lo envíe muchas veces
+				rBTNPress <= 0;
+				rCLKCounter <= 0;
+				rBTN <= rBTN;
+			end 
+			else begin //No hay BTN
+				rBTNListo <= rBTNListo;
+				rBTNPress <= rBTNPress;
+				rCLKCounter <= rCLKCounter;
+				rBTN <= rBTN;
+			end // end Manejo BTN
+		end //end Control Botones
+	end	//end Lógica principal
+	
+endmodule
---
+++ Boton/TestBench.v
@@ -0,0 +1,89 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:30:52 01/30/2011
+// Design Name:   MiniAlu
+// Module Name:   D:/Proyecto/RTL/Dev/MiniALU/TestBench.v
+// Project Name:  MiniALU
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: MiniAlu
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module TestBench;
+
+	// Inputs
+	reg Clock;
+	reg Reset;
+	
+	reg BTN_EAST; // Moverse Iquierda
+	reg BTN_NORTH; // Moverse Arriba
+	reg BTN_SOUTH; // Moverse Abajo
+	reg BTN_WEST; // Moverse Derecha
+	
+	reg ROT_CENTER; // Seleccionador
+	reg ROT_A; // Girar sentido Horario
+	reg ROT_B; // Girar sentido Anti Horario
+
+	// Outputs
+	wire VGA_RED, VGA_GREEN, VGA_BLUE,  // Colores VGA
+    wire VGA_HSYNC, // Cambio de fila VGA
+	wire VGA_VSYNC // Return inicio VGA 
+	wire [3:0] SF_DATA; // Datos para LCD
+
+	// Instantiate the Unit Under Test (UUT)
+	MiniAlu uut (
+		.Clock(Clock), 
+		.Reset(Reset),
+		.BTN_EAST(BTN_EAST),
+		.BTN_NORTH(BTN_NORTH),
+		.BTN_SOUTH(BTN_SOUTH),
+		.BTN_WEST(BTN_WEST),
+		.ROT_CENTER(ROT_CENTER),
+		.ROT_A(ROT_A)
+		.ROT_B(ROT_B),
+		.VGA_RED(VGA_RED), 
+		.VGA_GREEN(VGA_GREEN),
+		.VGA_BLUE(VGA_BLUE),
+		.VGA_HSYNC(VGA_HSYNC),
+		.VGA_VSYNC(VGA_VSYNC),
+		.SF_DATA(SF_DATA)
+	);
+	
+	always
+	begin
+		#5  Clock =  ! Clock;
+
+	end
+
+	initial begin
+		// Initialize Inputs
+		Clock = 0;
+		Reset = 0;
+		PS2_CLK = 1;
+		PS2_DATA = 1;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		Reset = 1;
+		#50
+		Reset = 0;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
