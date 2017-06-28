fbca123367ab2238b10ce62e772c17af6c8cde68
Luis Mora <lmoramora94@gmail.com>
2017-06-28 09:20:54 -0600

    CELDA_TOPO lista

---
+++ Proyecto_final/CELDA_TOPO.v
@@ -0,0 +1,22 @@
+module CELDA_TOPO(
+
+	input reset,
+	input wire PONER_TOPO,
+	input wire GOLPE,
+	input wire SELECT,
+	output wire HIT,
+	output wire [2:0] RGB
+	);
+
+	wire TOPO;
+	wire SELECTED;
+	wire oHIT;
+	assign TOPO = (reset)? 0 : (HIT)? 0: PONER_TOPO;
+	assign SELECTED = (reset)? 0 : SELECT;
+
+	// Si no esta seleccionado y no es topo, esta verde. Si no, si esta seleccionado es amarillo y si no esta seleccionado es topo.
+	assign RBG = (!SELECTED && !TOPO)? 3'b010: (SELECTED)? 3'b110: 3'b001;
+								    // (GREEN)			  (YELLOW) (BLUE)
+	assign HIT = (SELECTED && TOPO && GOLPE)? 1 : 0;
+
+endmodule
